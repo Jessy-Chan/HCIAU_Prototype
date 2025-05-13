720e7fbd0c1df64af375d27f5dfa56503baa335f
Chan Jing Yu <107841694+BlueWhale8581@users.noreply.github.com>
2025-05-13 15:04:36 +0800

    Edit vite host

--- nvadb-frontend/vite.config.js
+++ nvadb-frontend/vite.config.js
@@ -5,7 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 1235,
-    host: true
+    host: true,
+    allowedHosts: [
+      '7954489d-2573-46dc-af4d-2ccf16c539fe-00-1qihhwn7eh3h3.sisko.replit.dev',
+      'localhost'
+    ]
   },
   css: {
     postcss: './postcss.config.js',
